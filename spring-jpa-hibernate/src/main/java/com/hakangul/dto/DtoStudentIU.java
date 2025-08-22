da3e4ea44fb76bd5049de18fb588eedceea5160f
Hakan GUL <hakanngul@icloud.com>
2025-08-23 02:40:52 +0300

    Add Spring Boot Actuator dependency and implement global exception handling

--- spring-jpa-hibernate/src/main/java/com/hakangul/dto/DtoStudentIU.java
+++ spring-jpa-hibernate/src/main/java/com/hakangul/dto/DtoStudentIU.java
@@ -2,11 +2,15 @@ package com.hakangul.dto;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.validation.constraints.*;
 
 @Setter
 @Getter
---
+++ spring-jpa-hibernate/src/main/java/com/hakangul/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.hakangul.exception;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice // Bu anatasyon exception yakalamak için ekleniyor
+public class GlobalExceptionHandler {
+
+    private List<String> addMapValue(List<String> list, String newValue) {
+        list.add(newValue);
+        return list;
+    }
+
+    
+    //Spring validation dan fırlatılan hataları alıp yönetmek
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, List<String>> errorsMap = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            if(errorsMap.containsKey(fieldName)) {
+                errorsMap.put(fieldName, addMapValue(errorsMap.get(fieldName), message));
+            } else {
+                errorsMap.put(fieldName, addMapValue(new ArrayList<>(), message));
+            }
+        }); 
+       return ResponseEntity.badRequest().body(createApiError(errorsMap));
+    }
+
+    private ApiError createApiError(Map<String, List<String>> errors) {
+        ApiError apiError = new ApiError();
+        apiError.setId(UUID.randomUUID().toString());
+        apiError.setErrorTime(new Date());
+        apiError.setErrors(errors);
+        return apiError;
+    }
+
+}
