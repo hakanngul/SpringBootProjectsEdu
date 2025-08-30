efcbf620049108010ac1672821d7799c9d5f4418
Hakan GUL <hakanngul@icloud.com>
2025-08-30 13:19:50 +0300

    feat(exceptionManagment): initialize exception management module
    - Add Maven project configuration for exceptionManagment
    - Create main class with basic "Hello World!" output
    - Set Java compiler release version to 21
    - Define main class property for execution entry point

---
+++ exceptionManagment/src/main/java/com/hakangul/exception_managment/ExceptionManagment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hakangul.exception_managment;
+
+/**
+ *
+ * @author hakangul
+ */
+public class ExceptionManagment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
